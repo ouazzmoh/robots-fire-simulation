fe439d7c314ea5da2a26af64206f87d707145e9d
Akram <akram.zaki@grenoble-inp.org>
2022-10-30 23:26:45 +0100

    comments + getvoisin modified

--- src/Carte.java
+++ src/Carte.java
@@ -1,15 +1,33 @@
 
 public class Carte {
+	/**
+	 * classe Carte, caracterisée par la taille de ses cases (en mètres, et pas la taille de tracé en pixels) , son nombre de lignes
+	 * son nombres de colonnes qui sert à définir la matrice des cases, enfin la matrice des cases
+	 * qui contient les diffrentes cases.
+	 */
 	private int tailleCases;
 	private int nbLignes;
 	private int nbColonnes;
 	private Case[][] carte;
+	/**
+	 * un premier constructeur, qui crée un carte vide
+	 * @param tailleCases taille des cases en mètres
+	 * @param nbLignes nombre de cases dans chaque ligne
+	 * @param nbColonnes nombre de cases dans chaque colonne
+	 */
 	public Carte(int tailleCases, int nbLignes, int nbColonnes) {
 		this.tailleCases = tailleCases;
 		this.nbLignes = nbLignes;
 		this.nbColonnes = nbColonnes;
 		this.carte = new Case[nbLignes][nbColonnes];
 	}
+	/**
+	 * Un deuxième constructeur, qui crée une carte dont les cases existent déjà
+	 * @param tailleCases taille des cases en mètres
+	 * @param nbLignes nombre de cases dans chaque ligne
+	 * @param nbColonnes nombre de cases dans chaque colonne
+	 * @param carte la matrice de cases
+	 */
 	public Carte(int tailleCases, int nbLignes, int nbColonnes, Case[][] carte) {
 		this.tailleCases = tailleCases;
 		this.nbLignes = nbLignes;
@@ -31,6 +49,12 @@ public class Carte {
 	public void setCase(int lig, int col, Case new_case) {
 		carte[lig][col] = new_case;
 	}
+	/**
+	 * Méthode qui vérifie l'existence d'une case voisine dans la direction donnée
+	 * @param src la cases qu'on vérifie si elle a un voisin ou non
+	 * @param dir la direction du voisin eventuel
+	 * @return boolean true si le voisin existe, false sinon
+	 */
 	public boolean voisinExiste(Case src, Direction dir) {
 		switch(dir) {
 			case NORD:
@@ -57,11 +81,17 @@ public class Carte {
 				return false;
 		}	
 	}
+	/**
+	 * méthode qui nous permet d'accéder au voisin de la case src
+	 * @param src case qu'on veut accéder à son voisin
+	 * @param dir direction du voisin
+	 * @return le voisin s'il existe
+	 */
 	public Case getVoisin(Case src, Direction dir) {
 		int new_lig = src.getLigne();
 		int new_col = src.getColonne();
-		//TODO: Test si le voisin existe avant de le retourner
-		switch(dir) {
+		if (voisinExiste(src, dir)) {
+			switch(dir) {
 			case NORD:
 				new_lig -= 1;
 			case SUD:
@@ -72,5 +102,9 @@ public class Carte {
 				new_col += 1;
 		}
 		return carte[new_lig][new_col];
+		}
+		else {
+			throw new IllegalArgumentException("ce voisin n'existe pas!!!")
+		}
 	}
 }
