0f075129573b9c580a2628726b5f130e1310ee73
Akram <akram.zaki@grenoble-inp.org>
2022-10-31 12:20:10 +0100

    documentation

--- src/Rpatte.java
+++ src/Rpatte.java
@@ -1,8 +1,21 @@
 
 public class Rpatte extends Robot {
+	/**
+	 * la classe Rpatte, une classe fille de la classe Robot, les robots à pattes n'ont pas de reservoir
+	 */
+	
+	/**
+	 * Constructeur public, qui crée un nouveau Robot à pattes avec la vitesse par défaut 30 km/h
+	 * @param position case dans laquelle le robot se trouve
+	 */
 	public Rpatte(Case position) {
 		super(30, position);
 	}
+	/**
+	 * Constructeur public, crée un nouveau Robot à pattes avec une vitesse donnée 
+	 * @param vitesse vitesse du robot à pattes
+	 * @param position position dans laquelle le robot se trouve
+	 */
 	public Rpatte(double vitesse, Case position) {
 		super(vitesse, position);
 	}
